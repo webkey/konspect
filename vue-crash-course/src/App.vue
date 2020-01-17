c4f00d0de259ab5d5b74c7f12837fc3d0f9eeeff
sergey.ilchenko <s.ilchenko@imedia.by>
2020-01-17 18:37:29 +0300

    Create vue-crash-course

---
+++ vue-crash-course/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <h1>To do application</h1>
+    <hr>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-crash-course/src/components/TodoList.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <ul>
+
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "TodoList"
+  }
+</script>
+
+<style scoped>
+
+</style>
