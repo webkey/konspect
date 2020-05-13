6765d9c246fa225e7528a2d59a7de0d7814253c7
webkey <serhiy_i@ukr.net>
2020-05-13 09:18:03 +0300

    record.js

--- vue-crm-minin/src/store/record.js
+++ vue-crm-minin/src/store/record.js
@@ -15,7 +15,10 @@ export default {
       try {
         const uid = await dispatch('getUid');
         const records = (await firebase.database().ref(`/user/${uid}/records`).once('value')).val() || {};
+        // records - это объект из ключей вида -M0oTyYS1TbVqsg054Wr (формируюстся в Firebase автоматически)
         return Object.keys(records).map(key => ({...records[key], id: key}));
+        // Возвращаем не объект, а массив объектов. Было -M0oTyYS1TbVqsg054Wr: {key: value, ... и т.д.}, а стало [{key: value, ..., id: -M0oTyYS1TbVqsg054Wr и т.д.}]
+        // То есть ключи из объекта records переносим в поле id объектов - элементов нового массива
       } catch (e) {
         commit('setError', e)
         throw e
