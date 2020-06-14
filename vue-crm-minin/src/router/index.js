1e99063b32ff6efe3c9e7cbe914647b6ff93174a
webkey <serhiy_i@ukr.net>
2020-06-14 22:20:23 +0300

    Planning

--- vue-crm-minin/src/router/index.js
+++ vue-crm-minin/src/router/index.js
@@ -68,8 +68,19 @@ const router = new VueRouter({
 })
 
 router.beforeEach((to, from, next) => {
+  // Метод router.beforeEach() - это хук перехода по роуту
+  // https://router.vuejs.org/ru/guide/advanced/navigation-guards.html#%D0%B3n%D0%BE%D0%B1%D0%B0n%D1%8C%D0%BD%D1%8B%D0%B5-%D1%85%D1%83%D0%BA%D0%B8-%D0%B4%D0%BE-%D0%BD%D0%B0%D0%B2%D0%B8%D0%B3%D0%B0%D1%86%D0%B8%D0%BE%D0%BD%D0%BD%D1%8B%D1%85-%D1%85%D1%83%D0%BA%D0%BE%D0%B2
   const currentUser = firebase.auth().currentUser
+  // firebase.auth() - это объект firebase с данными по авторизации
+  // firebase.auth().currentUser - объект с данными текущего пользователя
+  // И если пользователь не авторизован, то поле firebase.auth().currentUser будет в значении null
+  // https://firebase.google.com/docs/auth/web/manage-users?hl=ru
   const requireAuth = to.matched.some(record => record.meta.auth)
+  // to.matched - это массив роутов относящихся к текущему URL (например, к /foo/bar могут относится две записи: как /foo/bar, так и /foo)
+  // Метод .some() - это метод массива, который определяет, удовлетворяет ли какой-либо элемент массива условию, заданному в передаваемой функции
+  // То есть метод .some перебирает в цикле все роуты относящиеся к текущему URL, и если хотя бы один из них имеет поле meta.auth со значением true,
+  // то вернет true, если нет, то вернет false
+  // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/some
 
   if (requireAuth && !currentUser) {
     next('/login?message=login')
--- vue-crm-minin/src/views/History.vue
+++ vue-crm-minin/src/views/History.vue
@@ -1,43 +1,68 @@
 <template>
-  <div>
-    <div class="page-title">
-      <h3>История записей</h3>
+    <div>
+        <div class="page-title">
+            <h3>История записей</h3>
+        </div>
+
+        <div class="history-chart">
+            <canvas></canvas>
+        </div>
+
+        <Loader v-if="loading" />
+
+        <p class="center" v-else-if="!records.length">Записей пока нет <router-link to="/record">Добавьте первую</router-link></p>
+
+        <section v-else>
+            <!--15.5-->
+            <HistoryTable :records="records" />
+        </section>
     </div>
-
-    <div class="history-chart">
-      <canvas></canvas>
-    </div>
-
-    <section>
-      <table>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Сумма</th>
-            <th>Дата</th>
-            <th>Категория</th>
-            <th>Тип</th>
-            <th>Открыть</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>1212</td>
-            <td>12.12.32</td>
-            <td>name</td>
-            <td>
-              <span class="white-text badge red">Расход</span>
-            </td>
-            <td>
-              <button class="btn-small btn">
-                <i class="material-icons">open_in_new</i>
-              </button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </section>
-  </div>
 </template>
+
+<script>
+export default {
+    name: 'history',
+    // В поле data инициализируем нужные модели и флаг loading
+    data: () => ({
+        loading: true,
+        records: [], // Поле Записей (по умолч. - это пустой массив)
+        categories: [] // Полу Категорий (по умолч. - это пустой массив)
+    }),
+    // В хуке mounted забираем с сервера Категории и Записи
+    // с помощью экшенов, которые уже использовали ранее (в Planning.vue)
+    async mounted() {
+        // Заносим полученные данные в ПРИВАТНЫЕ ПЕРЕМЕННЫЕ инстанса вью, либо их можно еще назвать МОДЕЛЯМИ:
+        // this.records и this.categories.
+        // Но сначала records нужно трансформировать для фронта.
+        // this.records = await this.$store.dispatch('fetchRecords')
+        // Для этого record заносим в локальную переменную, а в модель заносим уже результать трансформации, чтобы Вью не рендерил станицу два раза:
+        // сначала после получения данных с сервера, а потом после их трансформации
+        const records = await this.$store.dispatch('fetchRecords')
+        this.categories = await this.$store.dispatch('fetchCategories')
+        this.records = records.map(record => {
+            // 15.1/ С помощью оператора map на каждой итерации мы получаем каждый элемент массива
+            // (в records он является объектом с набором полей: amount, categoryId, date, description, id, type)
+            return {
+                // 15.2/ С помощью оператора spread (...) разворачиваем объект record: ...record
+                // Оператор спред позволяет к существующим полям каждого элемента record добавля на каждой итерации новые поля 15.3
+                ...record,
+                // 15.3/ Добавляем название категории по ее id (categoryId)
+                // Метод .find находить в массиве елемент-объек содержащий id равный categoryId, и возвращает найденный объект.
+                // После чего у полученного объект берем значение поля title
+                categoryName: this.categories.find(c => c.id === record.categoryId).title,
+                // 15.4/ Добавляем класс и текст типа трансакции.
+                // 15.4.1/ При определения класса цвета исп. тернарный оператор:
+                typeClass: record.type === 'income' ? 'green' : 'red',
+                // 15.4.2/ При определения текста типа операции также исп. тернарный оператор:
+                typeText: record.type === 'income' ? 'Доход' : 'Расход'
+            }
+        })
+        // После формирования нового объекта модели this.records, передаем ее в компонент HistoryTable 15.5
+        // 15.6/ После получения всех данных с сервера, переопределяем модель прелоадера
+        this.loading = false
+    },
+    components: {
+        HistoryTable: () => import('../components/HistoryTable')
+    }
+};
+</script>
